6e707d843e56b65a237d204917d1bc5346887657
raxvan <razvan.om@gmail.com>
2019-07-18 20:03:22 +0300

    * updated ci script.

--- build/_ci_test_linux_x86.sh
+++ build/_ci_test_linux_x86.sh
@@ -1,9 +1,15 @@
+#!/bin/bash -xe
+
 python3 prj.cllio_tests.py make linux
 cd build/cllio_tests_linux_make
 
 make config=debug_x32
 make config=debug_x64
 
-cd cllio_tests_linux_make
 ./bin/x32/Debug/_cllio_tests
-cd ..
+python3 -c "import civalidate; civalidate.files_equal('samples.bin','../../tests/samples.bin')"
+
+./bin/x64/Debug/_cllio_tests
+python3 -c "import civalidate; civalidate.files_equal('samples.bin','../../tests/samples.bin')"
+
+
