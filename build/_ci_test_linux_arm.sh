d94b958b934549acb18c45844c5ec3f7563145b4
raxvan <razvan.om@gmail.com>
2019-07-23 21:30:16 +0300

    * small rename to project file

--- build/_ci_test_linux_arm.sh
+++ build/_ci_test_linux_arm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -xe
 
-python3 prj.cllio_tests.py make linux
-cd build/cllio_tests_linux_make
+python3 prj.cllio_test.py make linux
+cd build/cllio_test_linux_make
 
 make config=debug_arm
 
-./bin/ARM/Debug/_cllio_tests
+./bin/ARM/Debug/_cllio_test
 python3 -c "import civalidate; civalidate.files_equal('samples.bin','../../tests/samples.bin')"
 
--- build/_ci_test_linux_x86.sh
+++ build/_ci_test_linux_x86.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -xe
 
-python3 prj.cllio_tests.py make linux
-cd build/cllio_tests_linux_make
+python3 prj.cllio_test.py make linux
+cd build/cllio_test_linux_make
 
 make config=debug_x32
 make config=debug_x64
 
-./bin/x32/Debug/_cllio_tests
+./bin/x32/Debug/_cllio_test
 python3 -c "import civalidate; civalidate.files_equal('samples.bin','../../tests/samples.bin')"
 
-./bin/x64/Debug/_cllio_tests
+./bin/x64/Debug/_cllio_test
 python3 -c "import civalidate; civalidate.files_equal('samples.bin','../../tests/samples.bin')"
 
 
