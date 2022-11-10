94f2ea3ecda12c150ef56ef4b0aab6263ec9091b
raxvan <razvan.om@gmail.com>
2022-11-10 16:47:55 +0000

    *fixed assert

--- incl/cllio_config.h
+++ incl/cllio_config.h
@@ -9,6 +9,8 @@
 #define CLLIO_FILE_READ_MAPVIEW // enable/remove file read map view
 
 #define CLLIO_ENABLE_ASSERT
+
+//--------------------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------------------------------------
 
 #if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || __cplusplus >= 201703L)
@@ -16,18 +18,20 @@
 #endif
 
 //--------------------------------------------------------------------------------------------------------------------------------
-
+//--------------------------------------------------------------------------------------------------------------------------------
 #if defined(CLLIO_TESTING)
 
 #	include <ttf.h>
 #	define CLLIO_ASSERT TTF_ASSERT
 
-#elif defined(CLLIO_WITH_DEV_PLATFORM)
+#elif defined(CLLIO_ENABLE_ASSERT)
+//--------------------------------------------------------------------------------------------------------------------------------
+#	if defined(CLLIO_WITH_DEV_PLATFORM)
 
-#	include <devtiny.h>
-#	define CLLIO_ASSERT DEV_ASSERT
+#		include <devtiny.h>
+#		define CLLIO_ASSERT DEV_ASSERT
 
-#else defined(CLLIO_ENABLE_ASSERT)
+#	else
 
 //--------------------------------------------------------------------------------------------------------------------------------
 
@@ -36,19 +40,21 @@ namespace cllio
 	extern "C++" void cllio_assert_failed(const char* file, const int line, const char* cond);
 }
 
-#	define CLLIO_ASSERT(_COND)                                  \
-		do                                                       \
-		{                                                        \
-			if (!(_COND))                                        \
-				cllio_assert_failed(__FILE__, __LINE__, #_COND); \
-		} while (false)
-#	define CLLIO_ASSERT_FALSE(CSTR_MSG)                       \
-		do                                                     \
-		{                                                      \
-			cllio_assert_failed(__FILE__, __LINE__, CSTR_MSG); \
-		} while (false)
+#		define CLLIO_ASSERT(_COND)                                  \
+			do                                                       \
+			{                                                        \
+				if (!(_COND))                                        \
+					cllio_assert_failed(__FILE__, __LINE__, #_COND); \
+			} while (false)
+#		define CLLIO_ASSERT_FALSE(CSTR_MSG)                       \
+			do                                                     \
+			{                                                      \
+				cllio_assert_failed(__FILE__, __LINE__, CSTR_MSG); \
+			} while (false)
+#	endif
+//--------------------------------------------------------------------------------------------------------------------------------
 #endif
-
+//--------------------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------------------------------------
 
 #ifndef CLLIO_ASSERT
