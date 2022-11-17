10bee1d1a512cf4a2bff27fd888e092a3fea65de
raxvan <razvan.om@gmail.com>
2022-11-17 16:40:20 +0000

    * added stream_io, a generic reader/writer

---
+++ incl/cllio_tools/stream_io.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "file_read_mapview.h"
+#include "../stdfile_stream_io.h"
+#include "../memory_stream_io.h"
+
+namespace cllio
+{
+
+	struct stream_data_view
+	{
+		const cllio::byte_t* data = nullptr;
+		const std::size_t    size = 0;
+	};
+
+	//--------------------------------------------------------------------------------------------------------------------------------
+
+	struct stream_reader
+	{
+	public:
+		stream_reader() = default;
+		stream_reader(const stream_reader&) = default;
+		stream_reader& operator=(const stream_reader&) = default;
+
+	public:
+		stream_reader(const cllio::file_read_mapview&);
+		stream_reader(const cllio::memory_rstream&);
+		stream_reader(cllio::stdfile_rstream_impl&);
+
+		stream_reader(std::FILE*);
+		stream_reader(std::istream&);
+
+		//TODO: stream_reader(cllio::memory_rstream&);
+	public:
+		stream_data_view read(std::vector<cllio::byte_t>& buffer); // append to buffer
+		stream_data_view read();									// can fail if used reader can't get direct memory
+
+	protected:
+		const cllio::file_read_mapview* m_map_file_handle = nullptr;
+		const cllio::memory_rstream*	m_memory_handle = nullptr;
+		cllio::stdfile_rstream_impl*	m_std_file_handle = nullptr;
+		std::FILE*						m_raw_file_handle = nullptr;
+		std::istream*					m_istream_handle = nullptr;
+	};
+
+	//--------------------------------------------------------------------------------------------------------------------------------
+
+	struct stream_writer
+	{
+	public:
+		stream_writer() = default;
+		stream_writer(const stream_writer&) = default;
+		stream_writer& operator=(const stream_writer&) = default;
+
+	public:
+		stream_writer(std::FILE*);
+		stream_writer(std::ostream&);
+		stream_writer(cllio::stdfile_wstream_impl&);
+
+		//TODO: stream_writer(cllio::memory_wstream&);
+
+	public:
+		void write(const void* data, const std::size_t sz);
+
+	protected:
+		cllio::stdfile_wstream_impl* m_std_file_handle = nullptr;
+		std::FILE*					 m_raw_file_handle = nullptr;
+		std::ostream*				 m_istream_handle = nullptr;
+	};
+
+	//--------------------------------------------------------------------------------------------------------------------------------
+}
---
+++ src/stream_io.cpp
@@ -0,0 +1,95 @@
+#include <cllio_tools/stream_io.h>
+
+namespace cllio
+{
+
+	stream_reader::stream_reader(const cllio::file_read_mapview& h)
+		: m_map_file_handle(&h)
+	{
+	}
+	stream_reader::stream_reader(const cllio::memory_rstream& h)
+		: m_memory_handle(&h)
+	{
+	}
+	stream_reader::stream_reader(cllio::stdfile_rstream_impl& h)
+		: m_std_file_handle(&h)
+	{
+	}
+	stream_reader::stream_reader(std::istream& h)
+		: m_istream_handle(&h)
+	{
+	}
+	stream_reader::stream_reader(std::FILE* h)
+		: m_raw_file_handle(h)
+	{
+	}
+
+	stream_data_view stream_reader::read(std::vector<cllio::byte_t>& buffer)
+	{
+		{
+			auto r = read();
+			if (r.size > 0)
+				return r;
+		}
+		std::size_t start = buffer.size();
+		if (m_std_file_handle != nullptr)
+		{
+			m_std_file_handle->read_into_container(buffer);
+		}
+		else if (m_raw_file_handle != nullptr)
+		{
+			auto pos = std::ftell(m_raw_file_handle);
+			/*EXPECTED_RETURN(0)*/ std::fseek(m_raw_file_handle, 0, SEEK_END);
+			std::size_t filesize = std::ftell(m_raw_file_handle);
+			/*EXPECTED_RETURN(0)*/ std::fseek(m_raw_file_handle, pos, SEEK_SET);
+			std::size_t sz = std::size_t(filesize) - std::size_t(pos);
+			buffer.resize(start + sz);
+			/*EXPECTED_RETURN(sz)*/ std::fread(buffer.data() + start, 1, sz, m_raw_file_handle);
+		}
+		else if (m_istream_handle != nullptr)
+		{
+			// TODO: check performance on this!
+			buffer.insert(buffer.end(), std::istreambuf_iterator<char>(*m_istream_handle), std::istreambuf_iterator<char>());
+		}
+
+		return stream_data_view(buffer.data() + start, buffer.size() - start);
+	}
+	stream_data_view stream_reader::read()
+	{
+		if (m_map_file_handle != nullptr)
+		{
+			return stream_data_view { static_cast<const cllio::byte_t*>(m_map_file_handle->data()), m_map_file_handle->size() };
+		}
+		if (m_memory_handle != nullptr)
+		{
+			return stream_data_view { m_memory_handle->data(), std::size_t(m_memory_handle->size()) };
+		}
+		return stream_data_view{};
+	}
+
+	//--------------------------------------------------------------------------------------------------------------------------------
+
+	stream_writer::stream_writer(cllio::stdfile_wstream_impl& h)
+		: m_std_file_handle(&h)
+	{
+	}
+	stream_writer::stream_writer(std::FILE* h)
+		: m_raw_file_handle(h)
+	{
+	}
+	stream_writer::stream_writer(std::ostream& h)
+		: m_istream_handle(&h)
+	{
+	}
+
+	void stream_writer::write(const void* data, const std::size_t sz)
+	{
+		if (m_std_file_handle != nullptr)
+			m_std_file_handle->write_raw_buffer(data, sz);
+		else if (m_raw_file_handle != nullptr)
+			std::fwrite(data, 1, sz, m_raw_file_handle);
+		else if (m_istream_handle != nullptr)
+			m_istream_handle->write((const char*)data, sz);
+	}
+
+}
